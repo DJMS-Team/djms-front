3c108a80ea6a35d54b14d0fd925e17b0e0855c50
Mateo Silva <1000035385@u.icesi.edu.co>
2024-06-15 17:32:45 -0500

    add Oauth configuration

---
+++ middleware.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server"
+import cookie from "cookie";
+
+
+export async function middleware(req: NextRequest){
+    const cookies = cookie.parse(req.headers.get("Cookie") || "");
+    const token = cookies.currentUser;
+    const tokenFromOauth = req.cookies.get("currentUser");
+
+
+    if(!token &&
+        !tokenFromOauth &&
+        req.nextUrl.pathname.startsWith('/home')
+    ){
+        console.log("cant enter home ");
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+
+    if (req.nextUrl.pathname.startsWith("/oauth")) {
+        const oAuthToken = req.nextUrl.searchParams.get("token") || "";
+        const oAuthid = req.nextUrl.searchParams.get("id") || ""
+        const oAuthEmail = req.nextUrl.searchParams.get("email") || ""
+    
+        
+        //console.log(tokenData);
+    
+        const current = {
+          id: oAuthid,
+          email:oAuthEmail,
+          token:oAuthToken
+        }
+    
+        //console.log(current)
+    
+        if (oAuthToken.length > 0) {
+          const response = NextResponse.redirect(new URL("/home", req.url));
+    
+          response.cookies.set({
+            name: "currentUser",
+            value: JSON.stringify(current),
+            maxAge: 60 * 60 * 24 * 7,
+          });
+          return response;
+        }
+      }
+    
+      return NextResponse.next()
+}
