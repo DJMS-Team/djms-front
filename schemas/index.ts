03230ae8b7d887171230d688a918619b21049f1f
JuanJoseLL <103261155+JuanJoseLL@users.noreply.github.com>
2024-06-17 12:06:45 -0500

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,9 +2,6 @@ import * as z from 'zod';
 
 
 export const RegisterSchema = z.object({
-
-export const LoginSchema = z.object({
-
     email: z.string().email({
         message: 'Please enter a valid email address'
     }),
@@ -15,7 +12,16 @@ export const LoginSchema = z.object({
     name: z.string().min(2,{
         message: 'Name is required'
     })
+    
+})
+export const LoginSchema = z.object({
 
-        message: 'Password is required'
+    email: z.string().email({
+        message: 'Please enter a valid email address'
     }),
+    password: z.string().min(6,{
+
+        message: 'Password must be at least 6 characters long'
+    })
+
 })
