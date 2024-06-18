6a74d75b906766acd477e9448545920a72fc102c
Mateo Silva <1000035385@u.icesi.edu.co>
2024-06-17 22:58:00 -0500

    add hooks and apis

---
+++ APIS/product.api.ts
@@ -0,0 +1,97 @@
+import axios, { AxiosInstance } from "axios";
+
+
+export class ProductApi {
+    protected readonly instance: AxiosInstance;
+    public constructor(url: string) {
+        this.instance = axios.create({
+          baseURL: url,
+          timeout: 30000,
+          timeoutErrorMessage: "Time out!",
+          headers: {
+            'Access-Control-Allow-Origin': '*', // Allow all origins
+          },
+          
+        });
+    }
+
+    createProduct = async (product_name:string, description:string, price:number, photo_url:string,product_category_id:string)=>{
+        try{
+            const res = await this.instance
+                .post(`/products`,{
+                    product_name: product_name,
+                    description: description,
+                    price: price, 
+                    photo_url: photo_url,
+                    product_category_id: product_category_id
+                })
+            
+            return res.data
+        }catch(error){
+            throw error
+        }
+    }
+
+    findProducts = async() => {
+        try{
+            const res = await this.instance
+                .get(`/products`)
+            return res.data
+        }catch(error){
+            throw error
+        }
+    }
+
+    findOneProduct = async(id:string,product_name:string, description:string, price:number, photo_url:string,product_category_id:string) => {
+        try{
+            const res = await this.instance
+                .get(`/products/${id}`)
+            return res.data;
+        }catch(err){
+            throw err
+        }
+    }
+
+    updateProduct = async (id:string,product_name:string, description:string, price:number, photo_url:string,product_category_id:string) =>{
+        try{
+            const res = await this.instance
+                .patch(`/products/${id}`,{
+                    product_name: product_name,
+                    description: description,
+                    price: price,
+                    photo_url: photo_url,
+                    product_category_id: product_category_id
+                })
+            return res.data;
+        }catch(error){
+            throw error
+        }
+    }
+
+    deleteProduct = async (id:string) => {
+        try{
+            const res = await this.instance
+                .delete(`/products/${id}`)
+        }catch(error){
+            throw error;
+        }
+    }
+
+    /*
+    filteredProducts = async (category:string,priceMin: number, priceMax: number, size:string) => {
+        try{
+            const res = await this.instance
+                .get(`/products/filter`,{
+                    category:category
+                })
+            
+            return res.data
+        }catch(error){
+            throw error
+        }
+    }
+    */
+
+
+
+}
