f28148cfc616b5f2aa3391fb4e7fb4e26352d851
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-19 17:56:25 -0500

    add billboard and footer component

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+export const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    ©copy: 2024 DJMS Store | All Rights Reserved
+                </p>
+            </div>
+        </footer>
+    )
+}
+
