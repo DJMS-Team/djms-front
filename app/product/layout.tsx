57504b5355d2c6c00685bb516042d9b4e7e2fd1c
svak <saravalcv@gmail.com>
2024-06-20 15:34:27 -0500

    landing to detail view

--- app/product/layout.tsx
+++ app/product/layout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "@/components/navbar";
 const LayoutProduct = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Navbar />
+
       <div className="min-h-screen bg-gray-100 pt-24 w-full pb-24">{children}</div>
     </div>
   );
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -4,6 +4,7 @@ import { Product } from "../../interfaces/product.interface"
 import IconButton from "./icon-button"
 import {  Expand, ShoppingCart } from "lucide-react"
 import Currency from "./currency"
+import { useRouter } from "next/navigation"
 
 interface ProductCard {
     data: Product
@@ -12,12 +13,15 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
-
+    const router = useRouter();
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
     return (
-          <div  className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 my-4 mx-2">
+          <div  onClick ={handleClick}className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
            <div className="aspect-sqare rounded-xl bg-gray-100 relative">
                 {/* <Image
-                  src={data.photo_url}
+                  src={data.}
                   fill
                   alt="Image"
                   className="aspect-square object-cover rounded-md"
