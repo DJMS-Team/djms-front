eaeec30603df8de9f7c285b3d90000879f2a733a
svak <saravalcv@gmail.com>
2024-06-20 11:19:33 -0500

    currency symbol

---
+++ hooks/use-cart.tsx
@@ -0,0 +1,11 @@
+<div className="absolute w-full px-6 transition opacity-0 group-hover:opacity-100 bottom-5">
+<div className="flex justify-center gap-x-6">
+    <IconButton
+        onClick={onPreview}
+        icon={<Expand size={20} className="text-gray-600" />}/>
+    <IconButton
+        onClick={onAddToCart}
+        icon={<ShoppingCart size={20} className="text-gray-600" />}/>
+</div>
+</div>
+</div>
