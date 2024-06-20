3cb39891468d656dcad9c4ff90afab9e3ea441f9
svak <saravalcv@gmail.com>
2024-06-19 23:12:59 -0500

    missing get connection

--- actions/get-products.ts
+++ actions/get-products.ts
@@ -1,25 +1,36 @@
-
+import { getAuthorizationHeader } from '@/APIS/getAuthorizationHeader';
 import axios from 'axios';
 
-interface Product {
-    id: string;
-    name: string;
-    email: string;
-    role: string;
+export interface Product {
+  id: string;
+  product_name: string;
+  description: string | null;
+  price: number;
+  photo_url: string;
+  size?: string;
+  order_details?: any[];
+  reviews?: any[];
+  product_category: any;
+  comments?: any[];
+  inventory: any;
+  seller: any;
 }
-export const getUsers = async (page = 1 , take = 10 , order = 'ASC') => {
-    try {
-        
-        const response = await axios.get(process.env.NEXT_PUBLIC_API_BASE_URL+`/products`, {   
 
-            }
-        ); 
-        const products = response.data;
-        return products
-        
-        
-      } catch (error) {
-        
-        console.error("Error fetching products:", error);
+export const getProducts = async (page = 1, take = 10, order = 'ASC') => {
+  try {
+    const response = await axios.get('http://localhost:3001/products', {
+      headers: getAuthorizationHeader(),
+      params: {
+        page,
+        take,
+        order
       }
+    });
+    const { data, meta } = response.data;
+    return { products: data, meta };
+
+  } catch (error) {
+    console.error('Failed to fetch products:', error);
+    return { products: [], meta: null };
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Container from "../components/ui/container";
 import {Billboard} from "../components/billboard";
-import getProducts from "../actions/get-products";
+import {getProducts} from "../actions/get-products";
 import ProductList from "@/components/product-list";
 const Home = async () => {
-  const products = await getProducts({isFeatured: true});
+  const products = await getProducts();
+  console.log(products);
   return (
     <Container>
       <div className="space-y-10 pb-10">
       <Billboard title="DMajor Store" photoUrl="https://firebasestorage.googleapis.com/v0/b/staynest-e6646.appspot.com/o/newyork.jpeg?alt=media&token=7e95f93d-d675-4c08-b261-7f2be6b46999" />
       </div>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products}/>
+      <ProductList title="Productos disponibles" items={products.products || []}/>
+
       </div>
     </Container>
   );
