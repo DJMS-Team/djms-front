d5b7a96ffd5aefc4361268de1a12b04a9816e99d
svak <saravalcv@gmail.com>
2024-06-20 15:43:15 -0500

    hotfix

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -18,14 +18,14 @@ const ProductCard: React.FC<ProductCard> = ({
         router.push(`/product/${data?.id}`)
     }
     return (
-          <div  onClick ={handleClick}className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+          <div  onClick ={handleClick}className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 my-4 mx-2">
            <div className="aspect-sqare rounded-xl bg-gray-100 relative">
-                {/* <Image
-                  src={data.}
+                 <Image
+                  src={data.photo_url}
                   fill
                   alt="Image"
                   className="aspect-square object-cover rounded-md"
-                /> */}
+                /> 
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex-gap-x-6 justify-center">
                         <IconButton 
