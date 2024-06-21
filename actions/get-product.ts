1ab32c5cf02a524224a8896655184d3f90050bf7
dylanbc1 <dyez1110@gmail.com>
2024-06-21 03:00:02 -0500

    feat: Add quantity for products in cart

--- actions/get-product.ts
+++ actions/get-product.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getProductById = async (id:string) => {
     
     try {
-        const response = await axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + `products/${id}`); 
+        const response = await axios.get(process.env.NEXT_PUBLIC_API_BASE_URL+`/products/${id}`); 
         const product = response.data
         return product
       } catch (error) {
