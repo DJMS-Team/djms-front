b9edd4bd98299001b70326132b828cc505d486f9
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-22 17:30:31 -0500

    add block and delete status to user

--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,7 @@ import { WelcomeMsg } from "@/components/welcome-msg"
 import { UserButton } from "./dashboard/user-button"
 export const Header = () => {
     return (
-        <header className="bg-gradient-to-b from-blue-700 to-blue-500
-         px-4 py-8 lg:px-14 pb-36">
+        <header className="bg-[#2A2A5A] px-4 py-8 lg:px-14 pb-36">
             <div className="max-w-screen-2xl mx-auto">
                 <div className="w-full flex items-center justify-between mb-14">
                     <div className="flex items-center lg:gap-x-16">
