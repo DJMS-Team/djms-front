a260bea19eaeb78f5d9819662934bae6063d5642
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-21 20:08:41 -0500

    add logout button to the admin page

---
+++ components/dashboard/user-button.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import {FaUser} from 'react-icons/fa';
+//import { ExitIcon, ReaderIcon, InfoCircledIcon } from '@radix-ui/react-icons'; 
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuContent
+} from '@/components/ui/dropdown-menu'
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback,
+} from '@/components/ui/avatar'
+import { LogoutButton } from './logout-button';
+import Link from 'next/link';
+import { LogOut } from 'lucide-react';
+
+
+
+export const UserButton = () => {
+
+    
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage/>
+                    <AvatarFallback className='bg-blue-500'>
+                        <FaUser className='text-white'/>
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-40' align='end'>                
+            
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <LogOut className='size-4'/>
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
