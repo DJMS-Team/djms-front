4233d3e1e23206de57a0e2db12aeabc2baeac5b2
dylanbc1 <dyez1110@gmail.com>
2024-06-23 17:58:18 -0500

    fix: Some bugs

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,20 +5,21 @@ import { IconMenu, IconSearch, IconShoppingCart } from "@tabler/icons-react";
 import { useCart } from "@/hooks/cart/use-cart";
 import { Button } from "./ui/button";
 import { LogOut, Menu, ShoppingCart, User } from "lucide-react";
-import { useRouter } from "next/navigation"; // Importa desde next/navigation
-import { usePathname } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { UserButton } from "./profile/user-button-page";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ProductCategory } from "@/interfaces/product-category.interface";
 import { getCategories } from "@/actions/get-categories";
 import { AiOutlineCaretDown } from "react-icons/ai";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { useMedia } from "react-use";
+import { useMedia } from "react-use"; // Ensure this is imported correctly
 import Link from "next/link";
 import { LogoutButton } from "./dashboard/logout-button";
 import Cookies from "js-cookie";
@@ -33,6 +34,9 @@ export const Navbar = () => {
   const [categories, setCategories] = useState<ProductCategory[]>([]);
   const [isOpen, setIsOpen] = useState(false);
 
+  const isMobile = useMedia("(max-width: 900px)", false); // Call this hook at the top level
+  const router = useRouter();
+
   let id: number | null = null;
   const currentUser = Cookies.get("currentUser");
 
@@ -44,9 +48,6 @@ export const Navbar = () => {
       console.error("Error parsing JSON:", error);
     }
   }
-
-  const router = useRouter();
-  const currentPath = usePathname();
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -83,11 +84,9 @@ export const Navbar = () => {
     const filters = { search: searchTerm };
     await getProductsFiltered(filters);
 
-    console.log('current path: ' + currentPath)
+    const currentPath = window.location.pathname;
 
-    if (currentPath === "/product/filter") {
-      router.push("/product/filter");
-    } else {
+    if (!(currentPath === "/product/filter")) {
       router.push("/product/filter");
     }
   };
@@ -96,14 +95,9 @@ export const Navbar = () => {
     return null;
   }
 
-  const isMobile = useMedia("(max-width: 900px)", false);
   if (isMobile) {
     return (
-      <nav
-        className={`${styles.navbar} ${
-          showNavbar ? styles.show : styles.hide
-        } px-4 flex justify-between items-center h-20`}
-      >
+      <nav className={`${styles.navbar} px-4 flex justify-between items-center h-20`}>
         <div className="flex-shrink-0">
           <a href="/" className="text-white text-2xl font-bold">
             DMajorStore
@@ -115,7 +109,7 @@ export const Navbar = () => {
             className="text-white flex items-center relative px-5 py-2"
           >
             <IconShoppingCart size={28} className="z-10" />
-            <span className="absolute bg-purple-dark text-white  z-[-10] rounded-full px-2 top-0 right-0">
+            <span className="absolute bg-purple-dark text-white z-[-10] rounded-full px-2 top-0 right-0">
               {cart.items.length}
             </span>
           </button>
@@ -124,15 +118,12 @@ export const Navbar = () => {
               <Button
                 variant="outline"
                 size="sm"
-                className="font-normal bg-white/10 hover:bg-white/202 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
+                className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
               >
                 <Menu className="h-8 w-8 text-white" />
               </Button>
             </SheetTrigger>
-            <SheetContent
-              side="left"
-              className="px-4 bg-[#1C1C3C] text-white border-none"
-            >
+            <SheetContent side="left" className="px-4 bg-[#1c1c3c] text-white border-none">
               <nav className="flex flex-col gap-y-8 pt-6 text-xl">
                 <form className={`flex ${styles.formContainer}`} onSubmit={handleSearchSubmit}>
                   <input
@@ -153,21 +144,16 @@ export const Navbar = () => {
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
                     {categories.map((category) => (
-                      <DropdownMenuItem
-                        onClick={() => router.push("/category/${category.id}")}
-                        key={category.id}
-                      >
+                      <DropdownMenuItem onClick={() => router.push(`/category/${category.id}`)} key={category.id}>
                         {category.category}
                       </DropdownMenuItem>
                     ))}
                   </DropdownMenuContent>
                 </DropdownMenu>
-                  <Link href={`/account/${id}`} className="flex items-center">
-                    Perfil
-                  </Link>
-                  <LogoutButton>
-                    Logout
-                  </LogoutButton>
+                <Link href={`/account/${id}`} className="flex items-center">
+                  Perfil
+                </Link>
+                <LogoutButton>Logout</LogoutButton>
               </nav>
             </SheetContent>
           </Sheet>
@@ -175,66 +161,71 @@ export const Navbar = () => {
       </nav>
     );
   }
+
   return (
     <nav
-      className={`${styles.navbar} ${showNavbar ? styles.show : styles.hide} z-10`}
+      className={`${styles.navbar} ${showNavbar ? styles.show : styles.hide} px-4 flex justify-between items-center h-20`}
     >
-      <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20 text-white">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <a href="/" className="text-white text-2xl font-bold">
-                DMajorStore
-              </a>
-            </div>
-            <form className={`flex ml-20 ${styles.formContainer}`} onSubmit={handleSearchSubmit}>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className={styles.searchInput}
-              />
-              <button type="submit" className={styles.searchButton}>
-                <IconSearch />
-              </button>
-            </form>
-          </div>
-          <div className="flex items-center justify-center flex-grow space-x-4">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex gap-2 justify-center items-center">
-                <p>Categorias</p>
-                <AiOutlineCaretDown size={12} className="mt-[1px]" />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                {categories.map((category) => (
-                  <DropdownMenuItem
-                    onClick={() => router.push("/category/${category.id}")}
-                    key={category.id}
-                  >
-                    {category.category}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <a href="/" className={`${styles.navLink} text-white`}>
-              Vender
-            </a>
-          </div>
-          <div className="flex items-center space-x-4">
-            <button
-              onClick={() => router.push("/cart")}
-              className="flex items-center relative px-5 py-2"
+      <div className="flex-shrink-0">
+        <a href="/" className="text-white text-2xl font-bold">
+          DMajorStore
+        </a>
+      </div>
+      <div className="flex gap-5 justify-center items-center">
+        <button
+          onClick={() => router.push("/cart")}
+          className="text-white flex items-center relative px-5 py-2"
+        >
+          <IconShoppingCart size={28} className="z-10" />
+          <span className="absolute bg-purple-dark text-white z-[-10] rounded-full px-2 top-0 right-0">
+            {cart.items.length}
+          </span>
+        </button>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger>
+            <Button
+              variant="outline"
+              size="sm"
+              className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
             >
-              <IconShoppingCart size={28} className="z-10" />
-              <span className="absolute bg-purple-dark text-white  z-[-10] rounded-full px-2 top-0 right-0">
-                {cart.items.length}
-              </span>
-            </button>
-            <UserButton />
-          </div>
-        </div>
+              <Menu className="h-8 w-8 text-white" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="px-4 bg-[#1c1c3c] text-white border-none">
+            <nav className="flex flex-col gap-y-8 pt-6 text-xl">
+              <form className={`flex ${styles.formContainer}`} onSubmit={handleSearchSubmit}>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className={`w-full flex-grow ${styles.searchInput}`}
+                />
+                <button type="submit" className={styles.searchButton}>
+                  <IconSearch />
+                </button>
+              </form>
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex gap-2 items-center">
+                  <p>Categorias</p>
+                  <AiOutlineCaretDown size={12} className="mt-[1px]" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  {categories.map((category) => (
+                    <DropdownMenuItem onClick={() => router.push(`/category/${category.id}`)} key={category.id}>
+                      {category.category}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Link href={`/account/${id}`} className="flex items-center">
+                Perfil
+              </Link>
+              <LogoutButton>Logout</LogoutButton>
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
-    );
+  );
 };
