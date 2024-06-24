848aa43655df2d2751b854461da0bdbd347500ff
Mateo Silva <1000035385@u.icesi.edu.co>
2024-06-24 01:35:32 -0500

    add reviews and orderDetail

--- components/productDetail/product-reviews.tsx
+++ components/productDetail/product-reviews.tsx
@@ -10,18 +10,19 @@ import {
 } from "@/components/ui/alert-dialog";
 import ReviewCard from "./review-card";
 import { Review } from "@/interfaces/review.interface";
+import { Product } from "@/interfaces/product.interface";
+import ReviewTotalCard from "./review-total-card";
 
 interface ProductReviewsProps {
   reviews: Review[] | undefined;
+  product: Product | null;
 }
 
-const ProductReviews: React.FC<ProductReviewsProps> = ({ reviews }) => {
+const ProductReviews: React.FC<ProductReviewsProps> = ({ reviews, product }) => {
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-2xl font-bold">Reviews</h2>
-      {reviews?.slice(0, 3).map((review, index) => (
-        <ReviewCard key={review.id || index} review={review} />
-      ))}
+        <ReviewTotalCard key={product?.id} product_id={product?.id} />
       <AlertDialog>
         <AlertDialogTrigger className="px-4 py-2 bg-primary text-white rounded-lg w-48">
           See all reviews
@@ -30,7 +31,7 @@ const ProductReviews: React.FC<ProductReviewsProps> = ({ reviews }) => {
           <AlertDialogHeader>
             <AlertDialogTitle>All reviews</AlertDialogTitle>
             <AlertDialogDescription className="max-h-[300px] overflow-auto text-black">
-              {reviews?.slice(3, reviews.length).map((review, index) => (
+              {reviews?.slice(0, reviews.length).map((review, index) => (
                 <ReviewCard key={review.id || index} review={review} />
               ))}
             </AlertDialogDescription>
