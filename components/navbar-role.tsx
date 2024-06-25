2c7aaf2413dcf4fede46be303e5a58923dfd4736
dylanbc1 <dyez1110@gmail.com>
2024-06-25 12:51:39 -0500

    feat: Compenetizing navbar

---
+++ components/navbar-role.tsx
@@ -0,0 +1,93 @@
+'use client';
+import React, { useEffect, useState } from "react";
+import styles from "./navbar.module.css";
+import { useCart } from "@/hooks/cart/use-cart";
+import { Button } from "./ui/button";
+import { UserButton } from "./profile/user-button-page";
+import Link from "next/link";
+import { LogoutButton } from "./dashboard/logout-button";
+
+interface NavbarRoleProps {
+    isMobile: boolean;
+    section: string;
+    currentUser: string | undefined;
+    idUser : number | null;
+}
+
+export const NavbarRole: React.FC<NavbarRoleProps> = ({isMobile, section, currentUser, idUser}: NavbarRoleProps) => {
+
+  if (isMobile) {
+    return (
+        <>
+            {section == 'vender' && currentUser? 
+            <a href="/" className={`${styles.navLink} text-white`}>
+                Vender
+            </a>
+        :
+            <></>
+        }
+
+        {section == 'vender' && !currentUser? 
+            <a href="/auth/login" className={`${styles.navLink} text-white`}>
+                Vender
+            </a>
+        :
+            <></>
+        }
+
+            {section == 'userButton' && !currentUser? 
+            <Button className={`bg-purple-dark ${styles.navLink} `}>
+                <Link href='/auth/login'>
+                    Log In
+                </Link>
+            </Button>
+                :
+                    <></>
+            }
+            
+            {section == 'userButton' && currentUser ?
+            <>
+                <Link href={`/account/${idUser}`} className="flex items-center">
+                Perfil
+              </Link>
+              <LogoutButton>Log Out</LogoutButton>
+            </>
+                :
+            <></>
+            }
+        </>
+    );
+  }
+
+  return (
+    <>
+        {section == 'vender' && currentUser? 
+            <a href="/" className={`${styles.navLink} text-white`}>
+                Vender
+            </a>
+        :
+            <></>
+        }
+
+        {section == 'vender' && !currentUser? 
+            <a href="/auth/login" className={`${styles.navLink} text-white`}>
+                Vender
+            </a>
+        :
+            <></>
+        }
+
+        {section == 'userButton' && currentUser? 
+            <UserButton />: <></>
+        }
+
+        {section == 'userButton' && !currentUser? 
+            <Button className={`bg-purple-dark ${styles.navLink}`}>
+            <Link href='/auth/login'>
+              Log In
+            </Link>
+            </Button>: <></>
+        }
+    </>
+  );
+};
