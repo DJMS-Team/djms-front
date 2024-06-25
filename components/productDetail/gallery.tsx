d3c362f503bf97da5c427b0fbdebf2fc08b40729
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-24 21:38:22 -0500

    add preview to cart

---
+++ components/productDetail/gallery.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Image from 'next/image'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { GalleryTab } from './gallery-tab'
+
+interface GalleryProps{
+    images: string
+}
+
+export const Gallery = ({images}: GalleryProps) => {
+
+    return (
+        <TabGroup as='div' className='flex flex-col-reverse'>
+            <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
+                <TabList className='grid grid-cols-4 gap-6'>
+                    {/* {images.map((image, index) => (
+                        <GalleryTab key={image} image={image}/>
+                    ))} */}
+                    <GalleryTab image={images}/>
+                </TabList>
+            </div>
+            <TabPanels className='aspect-square w-full'>
+                    {/* {images.map((image)=>(
+                        <TabPanel key={image.id}>
+                            <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden '>
+                                <Image
+                                 fill
+                                 src={image.url}
+                                 alt='image'
+                                 className='object-cover object-center' 
+                                 />
+                            </div>
+                        </TabPanel>
+                    ))} */}
+                    <TabPanel>
+                        <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden '>
+                            <Image
+                             fill
+                             src={images}
+                             alt='image'
+                             className='object-cover object-center' 
+                             />
+                        </div>
+                    </TabPanel>
+            </TabPanels>
+        </TabGroup>
+    )
+}
