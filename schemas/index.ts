49f23383990fcc08b8053dfc45b704503e8d03fe
DiegoZM1006 <1109184718@u.icesi.edu.co>
2024-06-25 01:59:35 -0500

    feat: add validations in register and login forms

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,14 +3,14 @@ import * as z from 'zod';
 
 export const RegisterSchema = z.object({
     email: z.string().email({
-        message: 'Please enter a valid email address'
+        message: 'Por favor ingresa un correo electronico valido'
     }),
     password: z.string().min(6,{
 
-        message: 'Password must be at least 6 characters long'
+        message: 'La contraseña debe tener al menos 6 caracteres'
     }),
     name: z.string().min(2,{
-        message: 'Name is required'
+        message: 'El nombre es requerido'
     })
     
 })
@@ -18,11 +18,11 @@ export const RegisterSchema = z.object({
 export const LoginSchema = z.object({
 
     email: z.string().email({
-        message: 'Please enter a valid email address'
+        message: 'Por favor ingresa un correo electronico valido'
     }),
     password: z.string().min(6,{
 
-        message: 'Password must be at least 6 characters long'
+        message: 'La contraseña debe tener al menos 6 caracteres'
     })
 
 })
